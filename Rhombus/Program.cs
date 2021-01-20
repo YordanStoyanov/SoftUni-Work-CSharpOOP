8d0718b989d5cff0a420441d5918fd51a16f22fa
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-01-20 16:10:54 +0200

    Add files via upload

---
+++ Rhombus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Rhombus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Rhombus rhombus = new Rhombus();
+            var rhombusDraw = rhombus.Draw(n);
+            Console.WriteLine(rhombusDraw);
+        }
+        
+    }
+}
---
+++ Rhombus/Rhombus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rhombus
+{
+    public class Rhombus
+    {
+        public string Draw(int countOfStars)
+        {
+            StringBuilder sb = new StringBuilder();
+            this.DrawTopPart(sb, countOfStars);
+            this.DrawLineOfStars(sb, countOfStars);
+            this.DrawBottonPart(sb, countOfStars);
+            return sb.ToString();
+        }
+        
+        private void DrawTopPart(StringBuilder sb, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+        }
+        private void DrawLineOfStars(StringBuilder sb, int numberOfStars)
+        {
+            for (int star = 0; star < numberOfStars; star++)
+            {
+                sb.Append('*');
+                if (star < numberOfStars - 1)
+                {
+                    sb.Append(' ');
+                }
+            }
+            sb.AppendLine();
+        }
+
+        private void DrawBottonPart(StringBuilder sb, int n)
+        {
+            for (int i = n - 1; i >= 1; i--)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+        }
+    }
+}
