bbeaedf84c58e21befa648c20478e6a3b240076f
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-04-21 17:37:45 +0300

    Add files via upload

---
+++ Encapsulation/AnimalFarm/AnimalFarm/Models/Chicken.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+        private string name;
+        private int age;
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            private set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    throw new Exception("Age should be between 0 and 15.");
+                }
+                this.age = value;
+            }
+        }
+        public double CalculateProductPerDay()
+        {
+            if (age > 0 && age <= 3)
+            {
+                return 1.5;
+            }
+            else if (age > 3 && age <= 7)
+            {
+                return 2;
+            }
+            else if (age > 7 && age <= 11)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0.75;
+            }
+        }
+    }
+    
+}
