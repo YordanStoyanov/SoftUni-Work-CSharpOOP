602c0569be8ed239322860c0939ff28e70ce3d5c
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-07-27 15:53:57 +0300

    Add files via upload

---
+++ UnitTestingLecture/Bank/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        private decimal amount;
+        private string name;
+
+        public BankAccount(string name, decimal amount)
+        {
+            this.Name = name;
+            this.Amount = amount;
+        }
+        public string Name { get; set; }
+
+        public decimal Amount
+        {
+            get
+            {
+                return this.amount;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Amount can not be 0!");
+                }
+                this.amount = value;
+            }
+        }
+    }
+}
---
+++ UnitTestingLecture/BankAccount.Test/BankAccountTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        private decimal amount;
+        private string name;
+        private BankAccount bankAccount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.name = "Pesho";
+            this.amount = 5;
+            this.bankAccount = new BankAccount(this.name, this.amount);
+        }
+
+        [Test]
+            public void WhenAccountInitializedWithPositiveValue_AmountIsChanged()
+        {
+            Assert.That(bankAccount.Amount, Is.EqualTo(this.amount));
+        }
+
+        [Test]
+        public void WhenAccountInitializedWithNegativeValue_AmountIsChanged()
+        {
+            amount = -5;
+            Assert.Throws<ArgumentException>(() => new BankAccount(name, this.amount));
+        }
+
+        [Test]
+        public void WhenAccountInitializedWithZeroValue_AmountIsChanged()
+        {
+            amount = 0;
+            bankAccount = new BankAccount(name, amount);
+            Assert.That(bankAccount.Amount, Is.EqualTo(this.amount));
+            //Assert.Throws<ArgumentException>(() => new BankAccount(name, amount));
+        }
+
+    }
+}
