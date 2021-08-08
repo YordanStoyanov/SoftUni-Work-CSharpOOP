63c0e33c047f7a27c44107bca94f0bfa58dfb31a
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-08-08 10:22:49 +0300

    Add files via upload

---
+++ C# OOP Retake Exam - 22 August 2020/EasterRaces/Models/Cars/SportsCar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars
+{
+    public class SportsCar : Car
+    {
+        private const double cubicCentimeters = 3000;
+        private const int minHorsePower = 250;
+        private const int maxHorsePower = 450;
+        /*The cubic centimeters for this type of car are 3000. Minimum horsepower is 250 and maximum horsepower is 450.
+If you receive horsepower which is not in the given range throw ArgumentException with message "Invalid horse power: {horsepower}.".
+
+         */
+        public SportsCar(string model, int horsePower) : 
+            base(model, horsePower, cubicCentimeters, minHorsePower, maxHorsePower)
+        {
+        }
+    }
+}
